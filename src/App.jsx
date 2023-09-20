df71a8b23beb73553028bc217b272617d90c90dd
Nagakburos <wansanice@proton.me>
2023-09-20 16:06:56 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from './Components/Hero';
+import Demo from './Components/Demo';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+
+
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,36 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+  <header className="w-full flex justify-center intems-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => Window.open('https://github.com/Nagakburos')}
+          className="black_btn"
+          >
+          
+          GitHub
+
+        </button>
+
+      </nav>
+      <h1 className="head_text">
+        Resuma <br /> Artigos com <br
+        className="max-md:hidden" />
+        <span
+        className="orange_gradient">OpenAi GPT-4</span>
+      </h1>
+      <h2 className="desc">Simplifique sua vida com RESUM, um resumidor de artigos que transforma textos longos em resumos claros e concisos!
+      </h2>
+
+
+
+
+    </header>  
+  )
+}
+
+export default Hero
