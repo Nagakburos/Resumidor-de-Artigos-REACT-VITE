dd6c565969122ca638f918a62ef36ebb6bdbc8b6
Nagakburos <wansanice@proton.me>
2023-09-21 15:13:56 -0300

    Correção de Erros
    Erro apresentado na linha:
    middleware: (getDefaultMiddleware) => getDefaultMiddleware().contact(articleApi.middleware)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' }, // Corrigido aqui
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
@@ -17,4 +17,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/Services/article.js
+++ src/Services/article.js
@@ -3,21 +3,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 export const articleApi = createApi({
-    reducerPath: 'articleApi',
-    baseQuery:fetchBaseQuery({
-        baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
-            headers.set('X-RapidAPI-Host',  'article-extractor-and-summarizer.p.rapidapi.com');  
-            
-            return headers;
-        }
+  reducerPath: 'articleApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', rapidApiKey);
+      headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `/summarize?url=${params.articleUrl}&length=3`,
     }),
-    endpoints: (builder) => ({
-        getSumary: builder.query({
-            query:(params) => '/summarize?url=${params.articleUrl}&length=3', 
-            return params
-                       
-        })
-    })
+  }),
 });
