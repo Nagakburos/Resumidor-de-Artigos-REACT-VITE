2e5138e53d10201ee88e6e3e29f974ebb515f4fc
Nagakburos <wansanice@proton.me>
2023-09-21 20:56:31 -0300

    correção de bug
    Correção de bug de resposta

--- src/Components/Demo.jsx
+++ src/Components/Demo.jsx
@@ -1,95 +1,111 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { linkIcon } from '../assets';
+import { linkIcon, loader, copy } from '../assets';
 import { useLazyGetSummaryQuery } from '../Services/article';
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url:'',
-    sumary:'',
+    url: '',
+    summary: '', // Corrigi o nome da chave para 'summary'
   });
   const [allArticles, setAllArticles] = useState([]);
 
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery ();
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
-    const articlesFromLocalStorage =JSON.parse(localStorage.getItem('articles')
-    )
-    if(articlesFromLocalStorage) {
-      setAllArticles(articlesFromLocalStorage)
-    }
+    const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
 
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
   }, []);
 
-  
-
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl:article.url });
+    const { data } = await getSummary({ articleUrl: article.url });
 
-    if(data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary }; // Corrigi a chave 'sumary' para 'summary'
       const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
 
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
-  }
-
+  };
 
   return (
     <section className="mt-16 w-full max-w-x1">
-      {/* search*/}
+      {/* search */}
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
           onSubmit={handleSubmit}
-          >
-            <img
+        >
+          <img
             src={linkIcon}
             alt="link_icon"
             className="absolute left-0 my-2 ml-3 w-5"
-            />
+          />
 
           <input
-          type="url"
-          placeholder="Enter a URL"
-          value={article.url}
-          onChange={(e) => setArticle({ ...article, url: e.target.value })}
-          required
-          className="url_input peer"
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
           />
 
           <button
             type="submit"
             className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
-            >
-              ➤
+          >
+            ➤
           </button>
-
         </form>
         {/* Browser URL History */}
-        <div className='flex floex-col gap-1 max-h-60 overflow-y-auto'>
-          {allArticles.map((item, index) => {
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
             <div
-              key={'link-${index}'}
-              onClick={() => setAllArticle(item)}
-              className='link_card'
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
             >
-              <div>
-                
-                </div>
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
               </div>
-          })}
-
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-      {/* Display Results */}    
+      {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black">
+            Uau, Parece que algo de errado não está certo...
+            <br />
+            <span>{error?.data?.error}</span>
+          </p>
+        ) : (
+          <p className="font-inter font-bold text-black">
+            Resultado: {article.summary}
+          </p>
+        )}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
